e7905d8b19360687f2dff54aa37de82a7b5b0761
kswhiting <kswhiting@gmail.com>
2020-11-26 22:43:12 +1300

    Added site header and general foundation stuff

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('.heading-h2').addClass('type-paragraph');
+    $('.logo').addClass('show-logo');
+    $('.arrow-svg').addClass('show-arrow');
+});
+
+
+$(window).scroll(function () {
+
+
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1) {
+        $("nav").removeClass("not-scrolled-nav");
+        $("nav").addClass("scrolled-nav");
+    } else {
+        $("nav").removeClass("scrolled-nav");
+        $("nav").addClass("not-scrolled-nav");
+    }
+
+    $(".header-image").css("top", "0" + (scroll / 1.8) + "px");
+
+});
+
+window.onscroll = function () { stickyNav() };
+
+    function stickyNav() {
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+
+var string = "Hey! I'm Kyle, an aspiring developer."; /* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("type-heading").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+    loopTimer = setTimeout('frameLooper()',70); /* change 70 for speed */
+}
+frameLooper();
+
